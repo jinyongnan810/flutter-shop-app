05c2c20a34e9ca54049562f9bb0cc8147bc2cda8
Kin Yuunan <yuunan.kin@gmail.com>
2021-08-16 06:32:13 +0900

    mixin and inheritance

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
 
+// difference between mixin(with) and inheritance(extends)
+// the class with its inheritance class has logical relationship, like human and man
+// while the class with its mixin has no such thing, and the class only uses the mixin for its utility properties
 class Products with ChangeNotifier {
   List<Product> _items = dummyProducts;
   List<Product> get items {
