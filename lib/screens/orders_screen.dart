6b417fc103e428b3e4a68813eb802073db93df29
Kin Yuunan <yuunan.kin@gmail.com>
2021-09-17 06:50:51 +0900

    use FutureBuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,57 +4,39 @@ import 'package:shop/components/main_drawer.dart';
 import 'package:shop/components/order_item.dart';
 import 'package:shop/providers/orders.dart';
 
-class OrderScreen extends StatefulWidget {
+class OrderScreen extends StatelessWidget {
   static final String routeName = '/orders';
-
-  @override
-  _OrderScreenState createState() => _OrderScreenState();
-}
-
-class _OrderScreenState extends State<OrderScreen> {
-  bool _isLoading = false;
-  Future<void> _load() async {
-    setState(() {
-      _isLoading = true;
-    });
-    Future(() async {
-      try {
-        await Provider.of<Orders>(context, listen: false).load();
-      } catch (error) {
-        print(error);
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('Error loading the orders.Please refresh again.'),
-          duration: Duration(seconds: 5),
-        ));
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-      }
-    });
-  }
-
-  @override
-  void initState() {
-    _load();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    final orderState = Provider.of<Orders>(context);
+    // final orderState = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Orders'),
       ),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).load(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) => OrderItem(orderState.get(index)),
-              itemCount: orderState.orders.length,
-            ),
+            );
+          } else {
+            if (dataSnapshot.hasError) {
+              print(dataSnapshot.error);
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Error loading the orders.Please refresh again.'),
+                duration: Duration(seconds: 5),
+              ));
+            }
+            return Consumer<Orders>(builder: (ctx, orderState, child) {
+              return ListView.builder(
+                itemBuilder: (ctx, index) => OrderItem(orderState.get(index)),
+                itemCount: orderState.orders.length,
+              );
+            });
+          }
+        },
+      ),
       drawer: MainDrawer(),
     );
   }
