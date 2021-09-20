b660d4dfb29fff0318a70df5a8f65a292fe568e6
Kin Yuunan <yuunan.kin@gmail.com>
2021-09-21 07:39:20 +0900

    extract common part of sign up/in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,47 +8,35 @@ class Auth with ChangeNotifier {
   DateTime? _expiryTime;
   String? _userId;
 
+  Future<void> authenticate(String email, String password, Uri uri) async {
+    final signupResponse = await http.post(uri,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(
+            {'email': email, 'password': password, 'returnSecureToken': true}));
+    print(signupResponse.body);
+    final authData = jsonDecode(signupResponse.body);
+    this._token = authData['idToken'];
+    this._userId = authData['localId'];
+    this._expiryTime = DateTime.now().add(Duration(hours: 1));
+  }
+
   Future<void> signup(String email, String password) async {
-    final url = Uri.parse(
+    final uri = Uri.parse(
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${dotenv.env['FIREBASE_API_KEY']}");
     try {
-      final signupResponse = await http.post(url,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode({
-            'email': email,
-            'password': password,
-            'returnSecureToken': true
-          }));
-      print(signupResponse.body);
-      final authData = jsonDecode(signupResponse.body);
-      this._token = authData['idToken'];
-      this._userId = authData['localId'];
-      this._expiryTime = DateTime.now().add(Duration(hours: 1));
+      authenticate(email, password, uri);
     } catch (error) {
       print(error);
     }
   }
 
   Future<void> signin(String email, String password) async {
-    final url = Uri.parse(
+    final uri = Uri.parse(
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${dotenv.env['FIREBASE_API_KEY']}");
     try {
-      final signupResponse = await http.post(url,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode({
-            'email': email,
-            'password': password,
-            'returnSecureToken': true
-          }));
-      print(signupResponse.body);
-      final authData = jsonDecode(signupResponse.body);
-      this._token = authData['idToken'];
-      this._userId = authData['localId'];
-      this._expiryTime = DateTime.now().add(Duration(hours: 1));
+      authenticate(email, password, uri);
     } catch (error) {
       print(error);
     }
