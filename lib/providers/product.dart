2df8f8b0880f026c54ed27976e07628f718677ef
Kin Yuunan <yuunan.kin@gmail.com>
2021-09-26 08:45:17 +0900

    fix favorite optimistic update

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:shop/models/http_exception.dart';
+
 class Product with ChangeNotifier {
   String id;
   String title;
@@ -16,17 +18,23 @@ class Product with ChangeNotifier {
       required this.price,
       required this.image,
       this.isFavorite = false});
-  Future<http.Response> toggleFavorite() async {
+  Future<void> toggleFavorite() async {
     this.isFavorite = !this.isFavorite;
     final url = Uri.https(
         "flutter-learning-36b57-default-rtdb.asia-southeast1.firebasedatabase.app",
         '/products/${this.id}.json');
     notifyListeners();
-    return http.patch(url, body: jsonEncode(this)).catchError((error) {
+    try {
+      final res = await http.patch(url, body: jsonEncode(this));
+      final authData = jsonDecode(res.body);
+      if (authData['error'] != null) {
+        throw HttpException(authData['error']['message']);
+      }
+    } catch (error) {
       this.isFavorite = !this.isFavorite;
       notifyListeners();
       throw error;
-    });
+    }
   }
 
   Product.fromJson(Map<String, dynamic> json)
