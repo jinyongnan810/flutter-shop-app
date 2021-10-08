92078c76872abf10a72469251c52a65909d213a4
Kin Yuunan <yuunan.kin@gmail.com>
2021-10-09 08:52:16 +0900

    use CustomRoute to change page transition effects

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/helpers/custom_route.dart';
 import 'package:shop/providers/auth.dart';
 import 'package:shop/screens/orders_screen.dart';
 import 'package:shop/screens/user_products_screen.dart';
@@ -26,7 +27,9 @@ class MainDrawer extends StatelessWidget {
             leading: Icon(Icons.history),
             title: Text('Orders'),
             onTap: () {
-              Navigator.of(context).pushReplacementNamed(OrderScreen.routeName);
+              // Navigator.of(context).pushReplacementNamed(OrderScreen.routeName);
+              Navigator.of(context).pushReplacement(
+                  CustomRoute(builder: (ctx) => OrderScreen()));
             },
           ),
           ListTile(
---
+++ lib/helpers/custom_route.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomRoute<T> extends MaterialPageRoute<T> {
+  CustomRoute({required WidgetBuilder builder, RouteSettings? settings})
+      : super(builder: builder, settings: settings);
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    // return super
+    //     .buildTransitions(context, animation, secondaryAnimation, child);
+
+    if (settings.name == '/') {
+      // if is the initial screen, do nothing
+      return child;
+    }
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
